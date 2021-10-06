b064d04cdd712e7d8c391491bbcf7d033e90e618
Rei-Fer <70172922+Rei-Fer@users.noreply.github.com>
2021-10-06 17:21:27 -0400

    Update settings.py

--- ProyectoTIS/ProyectoTIS/settings.py
+++ ProyectoTIS/ProyectoTIS/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': 'bdproyectotis',
             'USER': 'postgres',
-            'PASSWORD': '1DrKzP85T1',
+            'PASSWORD': '',
             'HOST': '127.0.0.1',
             'DATABASE_PORT': '5432',
         }
